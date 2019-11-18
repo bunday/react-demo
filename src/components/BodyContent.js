6c5bf3bedf275a54e5f3795ea1b4ddd4d3bb6af9
Zadat Olayinka <bundayy@outlook.com>
2019-11-18 17:14:07 +0100

    submission handler

--- src/components/BodyContent.js
+++ src/components/BodyContent.js
@@ -16,7 +16,12 @@ class BodyContent extends Component {
         this.setState({ [name]: value })
     }
 
-    
+    handleSubmission = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+    }
+
+
 
     render() {
         return (
@@ -24,26 +29,26 @@ class BodyContent extends Component {
                 <div className="py-4">
                     <h2 className="text-blue-500 text-2xl">Fetch Twitter History on Specific Dates</h2>
                 </div>
-               <div className="flex flex-wrap">
-               <div className="w-full lg:w-3/4">
-                <Form data={this.state} handleChange={this.handleChange}/>
-                <div className="flex flex-wrap w-full mt-4">
-                    <TweetCard/>
-                </div>
-                </div>
-                <div className="w-full lg:w-1/4">
-                    <div className="shadow-lg md:mt-4 sm:mt-4 lg:ml-4 py-4 px-2">
-                        <p className="text-blue-500 text-lg">Latest Trends</p>
-                        <p className="text-gray-400 text-sm">Checkout What is Trending</p>
-
-                        <div className="flex flex-wrap pt-4">
-                            <Trend/>
+                <div className="flex flex-wrap">
+                    <div className="w-full lg:w-3/4">
+                        <Form data={this.state} handleChange={this.handleChange} handleSubmission={this.handleSubmission} />
+                        <div className="flex flex-wrap w-full mt-4">
+                            <TweetCard />
                         </div>
                     </div>
-                   
+                    <div className="w-full lg:w-1/4">
+                        <div className="shadow-lg md:mt-4 sm:mt-4 lg:ml-4 py-4 px-2">
+                            <p className="text-blue-500 text-lg">Latest Trends</p>
+                            <p className="text-gray-400 text-sm">Checkout What is Trending</p>
+
+                            <div className="flex flex-wrap pt-4">
+                                <Trend />
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-               </div>
-                
+
             </main>
         )
     }
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,31 +2,32 @@ import React from 'react'
 
 const Form = (props) => {
     return (
-        <div className="shadow-md w-full py-4 px-2 flex flex-wrap">
-            <div className="w-full md:w-1/2 lg:w-1/4 p-2">
-                <label className="block text-gray-800 font-bold text-sm mb-2">
-                    Username
+        <div className="shadow-md w-full py-4 px-2">
+            <form className="flex flex-wrap" onSubmit={props.handleSubmission}>
+                <div className="w-full md:w-1/2 lg:w-1/4 p-2">
+                    <label className="block text-gray-800 font-bold text-sm mb-2">
+                        Username
                         </label>
-                <input placeholder="@bundayyo" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" value={props.data.username} name="username" onChange={props.handleChange} />
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 p-2">
-                <label className="block text-gray-800 font-bold text-sm mb-2">
-                    From
+                    <input placeholder="@bundayyo" type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" value={props.data.username} name="username" onChange={props.handleChange} />
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/4 p-2">
+                    <label className="block text-gray-800 font-bold text-sm mb-2">
+                        From
                         </label>
-                <input placeholder="01-01-2018" type="date" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" value={props.data.startDate} name="startDate" onChange={props.handleChange} />
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 p-2">
-                <label className="block text-gray-800 font-bold text-sm mb-2">
-                    To
+                    <input placeholder="01-01-2018" type="date" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" value={props.data.startDate} name="startDate" onChange={props.handleChange} />
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/4 p-2">
+                    <label className="block text-gray-800 font-bold text-sm mb-2">
+                        To
                         </label>
-                <input placeholder="01-01-2019" type="date" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" value={props.data.endDate} name="endDate" onChange={props.handleChange} />
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 p-2 flex justify-center">
-                <button className="bg-blue-500 px-4 py-2 rounded text-white mt-8">
-                    Fetch Tweets 🎉
+                    <input placeholder="01-01-2019" type="date" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" value={props.data.endDate} name="endDate" onChange={props.handleChange} />
+                </div>
+                <div className="w-full md:w-1/2 lg:w-1/4 p-2 flex justify-center">
+                    <button className="bg-blue-500 px-4 py-2 rounded text-white mt-8">
+                        Fetch Tweets 🎉
                         </button>
-            </div>
-
+                </div>
+            </form>
         </div>
     )
 }
