10d947a1a6511da8b95a3fe8b0ec6b865cdff95c
Zadat Olayinka <bundayy@outlook.com>
2019-11-18 17:06:27 +0100

    Trend exported

---
+++ src/components/Trend.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Trend = (props) => {
+    return (
+        <div className="text-blue-500 text-sm w-full md:w-1/3 lg:w-full">
+            <a href="#"> #WelcomeToNigeria <span className="text-gray-600"> 500k Tweets</span> </a>
+        </div>
+    )
+}
+
+export default Trend;
