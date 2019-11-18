fb72232c61ffa30c19c65559160ca772ce06a6d5
Zadat Olayinka <bundayy@outlook.com>
2019-11-18 12:58:12 +0100

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <header>
+           <div className="bg-blue-500 px-24 py-4 text-white">
+               Twitter Today in History
+           </div>
+        </header>
+    )
+}
+
+export default Header;
