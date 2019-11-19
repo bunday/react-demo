459971ac71a8695f3cb15ec70b852c2edb8c2742
Zadat Olayinka <bundayy@outlook.com>
2019-11-19 17:26:47 +0100

    updated to first api call

--- src/components/TwitterHistory.js
+++ src/components/TwitterHistory.js
@@ -11,6 +11,20 @@ class TwitterHistory extends Component {
         startDate: '',
         endDate: ''
     }
+    header = {
+        method: 'post',
+        headers: new Headers({
+            'Authorization':'Bearer AAAAAAAAAAAAAAAAAAAAABWwAwEAAAAAwpR5bj91XGI6HnNczREqg4isd5M%3DEZON7bu5E3dsGhDowTuta7itBPCwS2gpe0r5hHpZyiLJTluu6t',
+            'Content-Type':'application/json;charset=utf-8'
+        }),
+        body: this.data
+    }
+    data = {
+        "query":"from:bundayyo lang:en",
+        "maxResults": "100",
+        "fromDate":"201909010000", 
+        "toDate":"201911152359"
+    }
 
     handleChange = (event) => {
         const { name, value } = event.target;
@@ -20,6 +34,32 @@ class TwitterHistory extends Component {
     handleSubmission = (event) => {
         event.preventDefault();
         console.log(this.state)
+    }
+    componentDidMount() {
+        // let grant = new FormData()
+        // grant.append('grant_type', 'client_credentials');
+        // const oAuthHeader = {
+        //     method: 'post',
+        //     headers: new Headers({
+        //         'Authorization':'Basic WEpzbTRWM3Z1WVE1OWlZczBVUWlEMExKODpUbkpoSzZsbXRlaVB6YUl5d3F0N3dXcXBSUTZZaE84cExyVGdSaFZVYlRYZ2hxM1ZkRw==',
+        //         'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8'
+        //     }),
+        //     body: grant
+        // }
+        // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        // const url = 'https://api.twitter.com/oauth2/token'
+        // fetch(
+        //     proxyurl + url, 
+        //     oAuthHeader)
+        //     .then(jsonResponse => jsonResponse.json())
+        //     .then(response=> console.log(response));
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = 'https://api.twitter.com/1.1/tweets/search/fullarchive/Dev.json'
+        fetch(
+            proxyurl + url, 
+            this.header)
+            .then(jsonResponse => jsonResponse.json())
+            .then(response=> console.log(response));
     }
 
 
