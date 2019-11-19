be7ca6ac8ea0cc37095c60a0612aa9ab23e1474f
Zadat Olayinka <bundayy@outlook.com>
2019-11-19 17:32:20 +0100

    Updates

---
+++ precache-manifest.a10d49c004573b4ec80a83d03b72dd37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9116e2315af6ba17407c93867eadd5b6",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "c8a08097c2a55bb920d5",
+    "url": "/react-demo/static/css/main.220e1038.chunk.css"
+  },
+  {
+    "revision": "3d54ca21e829340741f7",
+    "url": "/react-demo/static/js/2.76097ac0.chunk.js"
+  },
+  {
+    "revision": "c8a08097c2a55bb920d5",
+    "url": "/react-demo/static/js/main.a7f354e5.chunk.js"
+  },
+  {
+    "revision": "915a3b492f9300d07352",
+    "url": "/react-demo/static/js/runtime-main.23002ce0.js"
+  },
+  {
+    "revision": "e1001140d103c9071742fb56b5197a40",
+    "url": "/react-demo/static/media/404.e1001140.svg"
+  },
+  {
+    "revision": "a2d1f06ee73c3a3a216b9603bd4b4930",
+    "url": "/react-demo/static/media/passport.a2d1f06e.jpg"
+  }
+]);
