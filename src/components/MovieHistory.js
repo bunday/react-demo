b6ea9bab2fb77dbac1b3dd95881559fbffa9e915
Zadat Olayinka <bundayy@outlook.com>
2020-02-11 10:29:44 +0100

    added action to fetch movies

--- src/components/MovieHistory.js
+++ src/components/MovieHistory.js
@@ -6,15 +6,7 @@ import Notification from "./Notification";
 import axios from "axios";
 
 const MovieHistory = () => {
-  const API_URL = "https://api.themoviedb.org/3/";
-  const API_KEY = "b5fe6f43e3714159d2209d0fe8c3a695";
-  const header = {
-    headers: new Headers({
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWZlNmY0M2UzNzE0MTU5ZDIyMDlkMGZlOGMzYTY5NSIsInN1YiI6IjU5MGIwOTc5OTI1MTQxNGZhODAxMzE2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ldVdrLz-iWpzmEgMvf_7IwT9pdfh14Pdqr_r2TGuMzg",
-      "Content-Type": "application/json;charset=utf-8"
-    })
-  };
+  
 
   const [movies, setMovies] = useState([]);
   const search = '';
